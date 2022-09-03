10b3be3b7665a7774f6e53f9cb4e3df74212fee7
Пользователь <Biybiruza>
2022-09-03 11:30:28 +0500

    I couldn't change the language

--- app/src/main/java/com/example/githubapp/LanguageActivity.kt
+++ app/src/main/java/com/example/githubapp/LanguageActivity.kt
@@ -1,29 +1,27 @@
 package com.example.githubapp
 
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import com.example.githubapp.databinding.ActivityLanguageBinding
+import java.util.*
 
 class LanguageActivity : AppCompatActivity() {
 
-    lateinit var languageCountry: LanguageCountry
     private lateinit var binding: ActivityLanguageBinding
-    val languageList = arrayOf("all", "English", "Karakalpak latin", "Karakalpak kirill", "Russion", "Uzbek")
+//    val languageList = arrayOf("all", "English", "Karakalpak latin", "Karakalpak kirill", "Russion", "Uzbek")
 
         override fun onCreate(savedInstanceState: Bundle?) {
+            loadLocate()
         super.onCreate(savedInstanceState)
+
+
+
         binding = ActivityLanguageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-//        binding.btnNext.setOnClickListener{
-//            val intent = Intent(this,MainActivity::class.java)
-//            startActivity(intent)
-//        }
 
             customSpinner()
     }
@@ -34,32 +32,73 @@ class LanguageActivity : AppCompatActivity() {
 
         binding.spinner.adapter = adapter
 
-        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
                 position: Int,
                 id: Long
             ) {
-//                languageCountry = LanguageCountry(position,Countries.countries[position])
-                if (Countries.countries[position] != "All Language")
-                    intentSelect()
-//                binding.spinner.adapter.apply {
-//                    if (binding.text.text == "All Language"){
-//                        intentSelect()
-//                    }
+//                if (Countries.countries[position] != "All Language"){
+
+                when(position){
+                    1 -> {
+                        setLocal("en")
+                        recreate()
+                        intentSelect()
+                    }
+                    2 -> {
+                        setLocal("kaa")
+                        recreate()
+                        intentSelect()
+                    }
+                    3 -> {
+                        setLocal("krc")
+                        recreate()
+                        intentSelect()
+                    }
+                    4 -> {
+                        setLocal("ru")
+                        recreate()
+                        intentSelect()
+                    }
+                    5 -> {
+                        setLocal("uz")
+                        recreate()
+                        intentSelect()
+                    }
+                }
+//
 //                }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 TODO("Not yet implemented")
             }
-
         }
     }
 
-    fun simpleSpinner(){
-        val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, languageList)
+    private fun setLocal(Lang: String){
+        val local = Locale(Lang)
+        Locale.setDefault(local)
+        val config = Configuration()
+        config.locale = local
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+
+        val editor = getSharedPreferences("Settings", MODE_PRIVATE).edit()
+        editor.putString("myLang",Lang)
+        editor.apply()
+    }
+
+    private fun loadLocate(){
+        val sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE)
+        val language = sharedPreferences.getString("MyLang", "")
+        setLocal(language!!)
+    }
+
+    /*fun simpleSpinner(){
+        val arrayAdapter = ArrayAdapter<String>(this,
+            R.layout.simple_spinner_dropdown_item, languageList)
         binding.spinner.adapter = arrayAdapter
         binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(
@@ -78,7 +117,7 @@ class LanguageActivity : AppCompatActivity() {
             }
 
         }
-    }
+    }*/
 
     fun intentSelect(){
         val intent = Intent(this,MainActivity::class.java)
