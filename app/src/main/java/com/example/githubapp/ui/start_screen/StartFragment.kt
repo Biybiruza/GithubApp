aff980a2a6e63d6031443d5c73e5c0772a6bf8d4
Biybiruza <ibashovabiybi@gmail.com>
2023-08-02 16:41:41 +0500

    github screen add

---
+++ app/src/main/java/com/example/githubapp/ui/start_screen/StartFragment.kt
@@ -0,0 +1,14 @@
+package com.example.githubapp.ui.start_screen
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.githubapp.R
+
+class StartFragment : Fragment(R.layout.fragment_start) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
